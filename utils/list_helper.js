2f893eaf41df779cb5ef03dcde40c1afc1d75619
Ash <nengxuj@qq.com>
2023-02-09 21:05:36 +0800

    Ex 4.4: test totalLikes

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,18 @@
+const blog = require("../models/blog")
+
 const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    let total = 0
+    for(const {likes} of blogs) {
+        total += likes
+    }
+    return total
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
